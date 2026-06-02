06ccd162f3c10b9ec42c23aaa4c6357a99f7debc
Jatinder Dadra <jatinderdadra48@gmail.com>
2026-06-02 22:16:53 +0530

    Responsive Design: Perfect layout for both laptop and mobile views.

--- Food_menu_temp/project.js
+++ Food_menu_temp/project.js
@@ -1,13 +1,19 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
-const menuClosenButton = document.querySelector("#menu-close-button");
-const menuList = document.querySelector(".nav-menu");
-const links = document.querySelectorAll(".nav-link")
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-link");
 
 menuOpenButton.addEventListener("click", () => {
-    document.body.classList.toggle("show-mobile-menu");    
+    document.body.classList.add("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener("click", () => menuOpenButton.click 
-());
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    });
+});
 
 
